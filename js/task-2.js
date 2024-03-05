0d16b83e9ca9bcc0cf0e15023752ca8ee308c75a
v.samopyt <v.samopyt14@gmail.com>
2024-03-05 20:29:48 +0200

    commit-00

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,14 @@
-function getShippingMessage (country,price,deliveryFee) {
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`
+function makeArray (firstArray, secondArray, maxLength) {
+    const newArray = firstArray.concat(secondArray);
+    if (newArray.length >= maxLength) {
+        return newArray.slice(0, maxLength);
+    }
+    return newArray;
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,15 @@
-function getElementWidth (content, padding, border) {
-    return Number.parseFloat(content, 10) + Number.parseFloat(padding, 10) * 2 + Number.parseFloat(border, 10) * 2;
+function  filterArray(numbers, value) {
+   const maxValueArray =[];
+   for (let item of numbers ) {
+    if (item > value) {
+        maxValueArray.push(item);
+    }
+   }
+   return maxValueArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
